d5c74ef4cab9b35c092179071cc16aca2c652c6d
thradams <thiago.adams@gmail.com>
2017-10-07 23:06:08 -0300

    set

--- CPrime/settings.h
+++ CPrime/settings.h
@@ -4,12 +4,6 @@
 
 #ifndef CPRIME
 
-//#define Array(T) { T *pData; int Size, Capacity;}
-//#define List(T) { T pHead; T pTail; }
-// #define Map(T) { MapItem2** pHashTable; unsigned int HashTableSize; int Count; }
-
-#define Union(...) { int type; }
-
 #define _defval(x)
 #define _default
 #define _auto
