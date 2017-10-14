1b41d435594e338053887d6f81859afe4fb94f16
thradams <thiago.adams@gmail.com>
2017-10-14 01:54:23 -0300

    Add files via upload

---
+++ cp/linuxconfig.h
@@ -0,0 +1,19 @@
+#define __restrict__
+#define __restrict
+#define __MINGW_IMPORT 
+
+#define _CRTIMP 
+#define __cdecl  
+#define __MINGW_NOTHROW 
+
+#define __inline__
+#define __x86_64__ 
+#define  __LP64__
+
+#define __GNUC_VA_LIST
+#define __gnuc_va_list char *
+        
+
+#pragma dir "/usr/include"
+#pragma dir "/usr/include/x86_64-linux-gnu"
+#pragma dir "/usr/lib/gcc/x86_64-linux-gnu/5/include"
