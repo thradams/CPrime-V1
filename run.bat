8bdb408418b9f1e533aa42c2a3a2012ae801d908
thradams <thiago.adams@gmail.com>
2017-10-22 18:41:13 -0200

    f

--- run.bat
+++ run.bat
@@ -1,17 +1,17 @@
 
-call "Release\CPrime.exe" -config "CPrime\vs2017.h" "CPrime\AllTemplates.c"
+
 call "Release\CPrime.exe" -config "CPrime\vs2017.h" "CPrime\Array.c"
-call "Release\CPrime.exe" -config "CPrime\vs2017.h" "CPrime\ArrayTemplate.c"
+
 call "Release\CPrime.exe" -config "CPrime\vs2017.h" "CPrime\Ast.c"
 call "Release\CPrime.exe" -config "CPrime\vs2017.h" "CPrime\AstPrint.c"
 call "Release\CPrime.exe" -config "CPrime\vs2017.h" "CPrime\BasicScanner.c"
 call "Release\CPrime.exe" -config "CPrime\vs2017.h" "CPrime\CodePrint.c"
 call "Release\CPrime.exe" -config "CPrime\vs2017.h" "CPrime\CPrime.c"
-call "Release\CPrime.exe" -config "CPrime\vs2017.h" "CPrime\ListTemplate.c"
+
 call "Release\CPrime.exe" -config "CPrime\vs2017.h" "CPrime\Macro.c"
 call "Release\CPrime.exe" -config "CPrime\vs2017.h" "CPrime\Map.c"
 call "Release\CPrime.exe" -config "CPrime\vs2017.h" "CPrime\Map2.c"
-call "Release\CPrime.exe" -config "CPrime\vs2017.h" "CPrime\MapTemplate.c"
+
 call "Release\CPrime.exe" -config "CPrime\vs2017.h" "CPrime\Parser.c"
 call "Release\CPrime.exe" -config "CPrime\vs2017.h" "CPrime\Path.c"
 call "Release\CPrime.exe" -config "CPrime\vs2017.h" "CPrime\PPToken.c"
@@ -21,5 +21,5 @@ call "Release\CPrime.exe" -config "CPrime\vs2017.h" "CPrime\Stream.c"
 call "Release\CPrime.exe" -config "CPrime\vs2017.h" "CPrime\StringEx.c"
 call "Release\CPrime.exe" -config "CPrime\vs2017.h" "CPrime\SymbolMap.c"
 call "Release\CPrime.exe" -config "CPrime\vs2017.h" "CPrime\TokenArray.c"
-call "Release\CPrime.exe" -config "CPrime\vs2017.h" "CPrime\UnionTemplate.c"
+
 call "Release\CPrime.exe" -config "CPrime\vs2017.h" "CPrime\UnitTest.c"
