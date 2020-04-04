eb6bc04a73ffcd5a94116ff9e73f59f291e48461
Thiago <thiago.adams@gmail.com>
2020-04-03 23:03:58 -0300

    command line

--- CPrime/buildall.bat
+++ CPrime/buildall.bat
@@ -5,6 +5,8 @@
  REM GERA AMALGAMATION
  call "Release\cprime.exe" -config "config.txt" -a CPrime.c -o ".\Generated\out.c"
 
+ REM cpc cprime.c -build -outDir Generated -o test.c
+
  REM GERA JS
 call "C:\Users\thiago\Source\Repos\emsdk\emsdk_env.bat"
 call emcc "Generated\out.c"  -o "Web\cprime.js"  -s WASM=0 -s EXPORTED_FUNCTIONS="['_CompileText']"  -s EXTRA_EXPORTED_RUNTIME_METHODS="['ccall', 'cwrap']"
